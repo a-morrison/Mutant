d8dccb3ec804503302ab8ec78ac76984ed5ab347
Alex Morrison <a.morrison17@gmail.com>
2019-02-18 20:26:52 -0500

    Create Init command

--- Mutant/Core/Commands/DeployCommand.cs
+++ Mutant/Core/Commands/DeployCommand.cs
@@ -9,16 +9,28 @@ namespace Mutant.Core.Commands
 {
     class DeployCommand : ConsoleCommand
     {
+
+        private bool IsNonSelective = false;
+        private bool RunAllTests = false;
+
         public DeployCommand()
         {
             this.IsCommand("Deploy", "Deploys changes");
 
             this.HasLongDescription("Extra info");
+
+            this.HasOption("a|all:", "Optional. If not used, tool defaults to selective deployment. Non-Selective deployment. Pushes all objects regardless of status.", 
+                v => IsNonSelective = v == null ? true : Convert.ToBoolean(v));
         }
 
         public override int Run(string[] remainingArguments)
         {
             Console.Out.WriteLine("Deploy called!");
+
+            if (IsNonSelective)
+            {
+                Console.Out.WriteLine("Deploying all objects!");
+            }
 
             return 0;
         }
---
+++ Mutant/Core/Commands/InitCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ManyConsole;
+
+namespace Mutant.Core.Commands
+{
+    class InitCommand : ConsoleCommand
+    {
+        private string URL;
+        private string Username;
+        private string Password;
+        private string Directory;
+
+        public InitCommand()
+        {
+            this.IsCommand("Init", "Initializes process with common parameters like the required username and password. Should be called first.");
+
+            this.HasRequiredOption("t|target-url=", "Required. URL of target org.", v => URL = v);
+            this.HasRequiredOption("u|username=", "Required. Username", v => Username = v);
+            this.HasRequiredOption("p|password=", "Required. Password", v => Password = v);
+            this.HasRequiredOption("d|working-directory", "Required. Full path of working directory.", v => Directory = v);
+        }
+
+        public override int Run(string[] remainingArguments)
+        {
+            return 0;
+        }
+    }
+}
