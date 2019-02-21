35d94b60f85bc0ce2d12a512067c9095e2094a3a
Alex Morrison <a.morrison17@gmail.com>
2019-02-21 18:42:17 -0500

    Init

--- Mutant/Core/Commands/InitCommand.cs
+++ Mutant/Core/Commands/InitCommand.cs
@@ -1,31 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ManyConsole;
+using Newtonsoft.Json;
 
 namespace Mutant.Core.Commands
 {
     class InitCommand : ConsoleCommand
     {
-        private string URL;
-        private string Username;
-        private string Password;
-        private string Directory;
+        private struct Info
+        {
+            public string URL;
+            public string Username;
+            public string Password;
+            public string WorkingDirectory;
+        }
+
+        Info MutantInfo = new Info();
 
         public InitCommand()
         {
             this.IsCommand("Init", "Initializes process with common parameters like the required username and password. Should be called first.");
 
-            this.HasRequiredOption("t|target-url=", "Required. URL of target org.", v => URL = v);
-            this.HasRequiredOption("u|username=", "Required. Username", v => Username = v);
-            this.HasRequiredOption("p|password=", "Required. Password", v => Password = v);
-            this.HasRequiredOption("d|working-directory", "Required. Full path of working directory.", v => Directory = v);
+            this.HasRequiredOption("t|target-url=", "Required. URL of target org.", v => MutantInfo.URL = v);
+            this.HasRequiredOption("u|username=", "Required. Username", v => MutantInfo.Username = v);
+            this.HasRequiredOption("p|password=", "Required. Password", v => MutantInfo.Password = v);
+            this.HasRequiredOption("d|working-directory=", "Required. Full path of working directory.", v => MutantInfo.WorkingDirectory = v);
         }
 
         public override int Run(string[] remainingArguments)
         {
+            Console.WriteLine(MutantInfo.URL);
+            Console.WriteLine(MutantInfo.WorkingDirectory);
+            Directory.SetCurrentDirectory(MutantInfo.WorkingDirectory);
+
+            using (StreamWriter file = File.CreateText(MutantInfo.WorkingDirectory + @"\.credentials"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, MutantInfo);
+            }
+
             return 0;
         }
     }
